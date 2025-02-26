aef896254cf3588892d66d48c20f5c0fc4a4898f
github personal <humbertpt14@gmail.com>
2025-02-26 15:54:58 -0500

    refactor(Buscando):Mejora del estado Buscando como circulo de carga

--- src/components/Buscando/Buscando.tsx
+++ src/components/Buscando/Buscando.tsx
@@ -3,8 +3,11 @@ import styles from "./Buscando.module.css";
 
 const Buscando: React.FC = () => {
   return (
-    <div className={styles.buscando}>Cargando...</div>
-  )
-}
+    <div className={styles.buscando} role="status" aria-live="polite">
+      <div className={styles.spinner}></div>
+      <p>Estamos buscando tu Numero de Conexión...</p>
+    </div>
+  );
+};
 
 export default Buscando;
